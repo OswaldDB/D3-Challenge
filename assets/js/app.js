85006017f754b2d4234ba711a0120786a2a809e9
OswaldDB <kevindeanathey@hotmail.com>
2021-01-06 18:26:34 -0600

    2020-01-06

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,136 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+function currencyFormat(num) {
+  return '$' + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+function percentageFormat(num) {
+  return num.toFixed(1).replace(/(\d)(?=(\d{3})+(?!\d))/g, '0.1%,') + "%"
+}
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select(".scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(stateData) {
+
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    stateData.forEach(function(data) {
+      data.income = +data.income;
+      data.obesity = +data.obesity;
+    });
+
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(stateData, d => d.income) * .9, d3.max(stateData, d => d.income) * 1.1])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([d3.min(stateData, d => d.obesity) * .9, d3.max(stateData, d => d.obesity) * 1.1])
+      .range([height, 0]);
+
+    // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(stateData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.income))
+    .attr("cy", d => yLinearScale(d.obesity))
+    .attr("r", "12")
+    .attr("fill", "light green")
+    .attr("opacity", ".5");
+    
+    var circleLabels = chartGroup.selectAll(null)
+      .data(stateData)
+      .enter()
+      .append("text");
+
+    circleLabels
+      .attr("x", function(d) {
+        return xLinearScale(d.income);
+      })
+      .attr("y", function(d) {
+        return yLinearScale(d.obesity);
+      })
+      .text(function(d) {
+        return d.abbr;
+      })
+      .attr("font-size", "9px")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "central")
+      .attr("fill", "white");
+
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([0, 30])
+      .html(function(d) {
+        return (`${d.state}<br>Average Income: ${currencyFormat(d.income)}<br>Obesity Rate: ${percentageFormat(d.obesity)}`);
+      });
+
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("mouseover", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Obesity Rate");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Average Income");
+  }).catch(function(error) {
+    console.log(error);
+  });
